9d783d331afddd02cddabd10779333c2beadc2e8
Relsola <sola001018@gmail.com>
2024-01-07 16:58:29 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ export { throttle, debounce } from './utils/throttle-debounce.js';
 export { isEqual } from './utils/isEqual.js';
 export { deepClone } from './utils/deepClone.js';
 export { once } from './utils/once.js';
+
+export { createUUID } from './simple/createUUID.js';
+export { curry } from './simple/curry.js';
+export { getSearchParams } from './simple/getSearchParams.js';
+export { memoize } from './simple/memoize.js';
+export { moneyFormat } from './simple/moneyFormat.js';
+export { omit } from './simple/omit.js';
+export { partial } from './simple/partial.js';
+export { pick } from './simple/pick.js';
+export { pipe } from './simple/pipe.js';
+export { typeOf } from './simple/typeOf.js';
+export { uniqueArrayObject } from './simple/uniqueArrayObject.js';
+export { zip } from './simple/zip';
+
+export { localCache, sessionCache } from './complex/myCache.js';
--- src/simple/curry.js
+++ src/simple/curry.js
@@ -4,7 +4,7 @@
  * @param {number} arity 参数数量
  * @returns {Function|any}
  */
-function curry(fn, arity = fn.length) {
+export function curry(fn, arity = fn.length) {
 	const curried = function (...rest) {
 		return rest.length >= arity ? fn(...rest) : (...args) => curried(...rest, ...args);
 	};
