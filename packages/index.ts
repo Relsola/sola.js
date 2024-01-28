aedee30458f663e46c17c97841fda44ee40bc14d
Relsola <sola001018@gmail.com>
2024-01-28 22:01:53 +0800

    update

---
+++ packages/index.ts
@@ -0,0 +1,19 @@
+export { default as createNanoEvents } from './more/nanoevents.js';
+export { throttle, debounce } from './complex/throttle-debounce.js';
+export { default as isEqual } from './simple/isEqual.js';
+export { default as deepClone } from './simple/deepClone.js';
+export { default as once } from './simple/once.js';
+export { default as createUUID } from './simple/createUUID.js';
+export { default as curry } from './simple/curry.js';
+export { default as getSearchParams } from './simple/getSearchParams.js';
+export { default as memoize } from './simple/memoize.js';
+export { default as moneyFormat } from './simple/moneyFormat.js';
+export { default as omit } from './simple/omit.js';
+export { default as partial } from './simple/partial.js';
+export { default as pick } from './simple/pick.js';
+export { pipe, compose } from './simple/pipe.js';
+export { default as typeOf } from './simple/typeOf.js';
+export { default as uniqueArrayObject } from './simple/uniqueArrayObject.js';
+export { default as zip } from './simple/zip.js';
+
+export { localCache, sessionCache } from './complex/myCache.js';
