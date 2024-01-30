c2f84edae33abe8264fa02e46fc1cc64a1c3d219
Relsola <sola001018@gmail.com>
2024-01-30 23:11:49 +0800

    update

--- packages/simple/getSearchParams.ts
+++ packages/simple/getSearchParams.ts
@@ -1,12 +1,8 @@
-/**
- * @description 获取 url 查询参数
- * @param 解析的字符串
- */
-export function getSearchParams(url?: string): Record<string, string> {
+export const getSearchParams = (url?: string): Record<string, string> => {
 	url ||= window.location.search;
 	const params: Record<string, string> = {};
-	for (const [key, value] of new URLSearchParams(url).entries()) {
+	new URLSearchParams(url).forEach((value: string, key: string) => {
 		params[key] = value;
-	}
+	});
 	return params;
-}
+};
